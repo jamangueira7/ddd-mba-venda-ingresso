ac4289434174e35bc9a7853474d83ccdfde121c8
João Mangueira <jamjs7@gmail.com>
2024-07-24 11:53:19 -0300

    add entidades e fazendo os primeiros testes passarem

--- src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
@@ -2,8 +2,22 @@ import Cpf from '../../../../common/domain/value-objects/cpf.vo'
 import { Customer, CustomerId } from '../customer.entity'
 
 test('deve criar um cliente', () => {
-    Customer.create({
+    let customer = Customer.create({
         name: 'João',
-        cpf: '993.464.838-50'
+        cpf: '439.562.180-51'
     })
+
+    expect(customer).toBeInstanceOf(Customer)
+    expect(customer.id).toBeDefined()
+    expect(customer.id).toBeInstanceOf(CustomerId)
+    expect(customer.name).toBe('João')
+    expect(customer.cpf.value).toBe('43956218051')
+
+    let customer2 = new Customer({
+        id: new CustomerId(customer.id.value),
+        name: 'João xpto',
+        cpf: new Cpf('527.337.550-99')
+    })
+
+    expect(customer.id.value).toBe(customer2.id.value)
 })
